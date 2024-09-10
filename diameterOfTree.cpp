2d3c4bd71aea5b8adc3dc6f92b412f9e6b5d3f7c
Satyam8932 <businesswithsatyam9555@gmail.com>
2024-09-11 02:13:40 +0530

    incorrect max diameter

--- diameterOfTree.cpp
+++ diameterOfTree.cpp
@@ -18,26 +18,26 @@ class Node {
 
 class Solution {
     public:
-    int maxDepth(Node* node) {
-        if (node == nullptr) {
-            return 0;
-        }
+    // int maxDepth(Node* node) {
+    //     if (node == nullptr) {
+    //         return 0;
+    //     }
         
-        int leftHeight = maxDepth(node->left);
-        int rightHeight = maxDepth(node->right);
+    //     int leftHeight = maxDepth(node->left);
+    //     int rightHeight = maxDepth(node->right);
         
-        return max(leftHeight, rightHeight) + 1;
-    }
+    //     return max(leftHeight, rightHeight) + 1;
+    // }
 
-    int maxDiameter(Node* node){
-        if (node == nullptr) {
-            return 0;
-        }
-        int diameterLeft = maxDiameter(node->left);
-        int diameterRight = maxDiameter(node->right);
-        int completeHeight =  maxDepth(node->left) + maxDepth(node->right) + 1;
+    // int maxDiameter(Node* node){
+    //     if (node == nullptr) {
+    //         return 0;
+    //     }
+    //     int diameterLeft = maxDiameter(node->left);
+    //     int diameterRight = maxDiameter(node->right);
+    //     int completeHeight =  maxDepth(node->left) + maxDepth(node->right) + 1;
 
-        int ans = max(completeHeight, max(diameterRight, diameterLeft));
-        return ans;
-    }
+    //     int ans = max(completeHeight, max(diameterRight, diameterLeft));
+    //     return ans;
+    // }
 };

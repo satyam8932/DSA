f8e689c687ab5448dda4498ed6f06d8bef89c5b1
Satyam8932 <businesswithsatyam9555@gmail.com>
2024-09-13 16:43:39 +0530

    diameter of tree

--- diameterOfTree.cpp
+++ diameterOfTree.cpp
@@ -18,26 +18,31 @@ class Node {
 
 class Solution {
     public:
-    // int maxDepth(Node* node) {
-    //     if (node == nullptr) {
-    //         return 0;
-    //     }
+    // Helper function to compute both the height and the diameter of the tree simultaneously
+    pair<int, int> heightAndDiameter(Node* node) {
+        // Base case: if the node is null, height is 0 and diameter is 0
+        if (node == nullptr) {
+            return {0, 0};
+        }
         
-    //     int leftHeight = maxDepth(node->left);
-    //     int rightHeight = maxDepth(node->right);
+        // Recursively get the height and diameter of the left and right subtrees
+        pair<int, int> leftResult = heightAndDiameter(node->left);
+        pair<int, int> rightResult = heightAndDiameter(node->right);
         
-    //     return max(leftHeight, rightHeight) + 1;
-    // }
+        // The height of the current node is the max height of the two subtrees plus 1
+        int height = max(leftResult.first, rightResult.first) + 1;
+        
+        // The diameter passing through the current node is the sum of the heights of the left and right subtrees
+        int diameterThroughNode = leftResult.first + rightResult.first;
+        
+        // The overall diameter is the maximum of the diameter through the current node and the diameters of the left and right subtrees
+        int diameter = max(diameterThroughNode, max(leftResult.second, rightResult.second));
+        
+        return {height, diameter};
+    }
 
-    // int maxDiameter(Node* node){
-    //     if (node == nullptr) {
-    //         return 0;
-    //     }
-    //     int diameterLeft = maxDiameter(node->left);
-    //     int diameterRight = maxDiameter(node->right);
-    //     int completeHeight =  maxDepth(node->left) + maxDepth(node->right) + 1;
-
-    //     int ans = max(completeHeight, max(diameterRight, diameterLeft));
-    //     return ans;
-    // }
+    int diameterOfBinaryTree(Node* node) {
+        // We are only interested in the diameter, so return the second value of the pair
+        return heightAndDiameter(node).second;
+    }
 };

694f17a4225dabfbde1176f6e9b1f0352b8871cb
Satyam Singh <businesswithsatyam9555@gmail.com>
2024-10-13 18:55:35 +0530

    move zero to end

--- python.py
+++ python.py
@@ -15,6 +15,32 @@ class Arrays:
         for element in array:
             print(element, end=" ")
         print()
+    
+    def FindMax(self, array):
+        """Find the maximum element in the array"""
+
+        max_value = array[0]
+        for element in array:
+            if element > max_value:
+                max_value = element
+        return max_value
+    
+    def FindMin(self, array):
+        """Find the minimum element in the array"""
+
+        min_value = array[0]
+        for element in array:
+            if element < min_value:
+                min_value = element
+        return min_value
+    
+    def MoveZeroEnd(self, array):
+        """Move all zero elements to the end of the array"""
+        size = len(array)
+        for i in range(size):
+            if array[i] == 0:
+                array.append(array.pop(i))
+        
 
 arryas = Arrays()
 
@@ -24,3 +50,9 @@ arryas.PrintArray(arr)
 arryas.ReverseArray(arr)
 print("Reversed Array:")
 arryas.PrintArray(arr)
+maximum = arryas.FindMax(arr)
+minimum = arryas.FindMin(arr)
+print(minimum, maximum)
+arr1 = [0, 1, 0, 3, 12]
+arryas.MoveZeroEnd(arr1)
+arryas.PrintArray(arr1)
